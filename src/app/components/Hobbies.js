5a92579f5546f478b59a037abcbac71fe85096f2
vigneshsankardeveloper <vigneshsankardeveloper@gmail.com>
2025-11-19 00:04:45 +0530

    Tech skills and hobbies additions

--- src/app/components/Hobbies.js
+++ src/app/components/Hobbies.js
@@ -7,12 +7,6 @@ import {
 export default function Hobbies() {
   const items = [
     {
-      title: "Learning New AI Tools",
-      image:
-        "https://images.unsplash.com/photo-1732310216648-603c0255c000?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      className: "absolute top-10 left-[20%] rotate-[-5deg]",
-    },
-    {
       title: "Books ( Novels )",
       image:
         "https://images.unsplash.com/photo-1697909623564-3dae17f6c20b?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -21,14 +15,20 @@ export default function Hobbies() {
     {
       title: "Documentaries",
       image:
-        "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "/documentries.png",
       className: "absolute top-5 left-[40%] rotate-[8deg]",
     },
     {
-      title: "Japan",
-      image:
-        "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Techonologies and Tools",
+      image:"/techies.png",
+        
       className: "absolute top-32 left-[55%] rotate-[10deg]",
+    },
+    {
+      title: "Learning New AI Tools",
+      image:
+        "/ai.png",
+      className: "absolute top-10 left-[20%] rotate-[-5deg]",
     },
   ];
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,15 @@ import { skills } from "../../utils/staticVariables";
 import { WorkExperinece } from "./components/WorkExperinece";
 import Hobbies from "./components/Hobbies";
 import Projects from "./components/Projects";
+import TechMarque from "./components/TechMarque";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col bg-gradient-to-b from-black via-purple-900 to-black">
       <HeroPage />
-      <SkillsCard skillContent={skills} />
+      <TechMarque />
       <WorkExperinece />
+      <SkillsCard skillContent={skills} />
       <Projects />
       <div className="pt-30">
         <Hobbies />
